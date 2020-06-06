ea4d4d9518289b78d6077ba2eb43d881c087d27a
Oleksandr Andrushchenko <andr2000@gmail.com>
2020-06-06 14:55:56 +0300

    Add settings activity
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- app/src/main/java/com/a2k/vncserver/MainActivity.java
+++ app/src/main/java/com/a2k/vncserver/MainActivity.java
@@ -5,18 +5,24 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
     public static final String TAG = "vncserver";
     private static final int PERMISSION_CODE = 1;
 
@@ -26,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private VncProjectionService mVncProjectionService;
 
     private int mCurrentRotation;
+
+    private boolean mDimBrightness;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,16 @@ public class MainActivity extends AppCompatActivity {
 
         mMediaProjectionManager = (MediaProjectionManager) getSystemService(
                 Context.MEDIA_PROJECTION_SERVICE);
+
+        setupSharedPreferences();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 
     @SuppressLint("MissingSuperCall")
@@ -94,6 +112,45 @@ public class MainActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         handleRotationChange();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this,
+                    SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void getPreferenceBrightness(SharedPreferences sharedPreferences) {
+        mDimBrightness = sharedPreferences.getBoolean(
+                SettingsActivity.CLIENT_BRIGHTESS_KEY,true);
+    }
+
+    private void setupSharedPreferences() {
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+        getPreferenceBrightness(sharedPreferences);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.endsWith(SettingsActivity.CLIENT_BRIGHTESS_KEY)) {
+            getPreferenceBrightness(sharedPreferences);
+        }
     }
 
     private ServiceConnection mVncConnection = new ServiceConnection()
